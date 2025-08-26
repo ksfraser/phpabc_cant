40ef21689de1765dfc1dfa7095b9eeef5bed663c
Kevin Fraser <kevin@ksfraser.ca>
2025-08-26 16:32:28 -0600

    process config

---
+++ src/Ksfraser/PhpabcCanntaireachd/AbcProcessorConfig.php
@@ -0,0 +1,9 @@
+<?php
+namespace Ksfraser\PhpabcCanntaireachd;
+
+class AbcProcessorConfig {
+    public $voiceOutputStyle = 'grouped'; // 'grouped' or 'interleaved'
+    public $interleaveBars = 1; // X bars per voice before switching (if interleaved)
+    public $barsPerLine = 4; // How many bars per ABC line
+    public $joinBarsWithBackslash = false; // true: use \ to join bars, false: one line per typeset line
+}
