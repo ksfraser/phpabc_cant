92bb7d3de6fd259eda8106c2045e555cbde98e57
Kevin Fraser <kevin@ksfraser.ca>
2025-08-26 16:16:31 -0600

    Fix tests

--- tests/TokenDictionaryTest.php
+++ tests/TokenDictionaryTest.php
@@ -1,57 +1,54 @@
 <?php
+
 namespace Ksfraser\PhpabcCanntaireachd\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Ksfraser\PhpabcCanntaireachd\Dict2php;
+use Ksfraser\PhpabcCanntaireachd\TokenDictionary;
 
 class TokenDictionaryTest extends TestCase
 {
     public function testPrepopulationFromAbcDict()
     {
-        // Simulate loading from abc_dict.php
-        $dict = Dict2php::loadDict();
-        $tokens = Dict2php::loadTokenTable();
+        $dict = [
+            'A' => ['cannt_token' => 'C', 'bmw_token' => 'B', 'description' => 'desc'],
+            'B' => ['cannt_token' => 'CC', 'bmw_token' => 'BB', 'description' => 'desc2'],
+        ];
+        $td = new TokenDictionary();
+        $td->prepopulate($dict);
         foreach ($dict as $abc => $row) {
-            $found = false;
-            foreach ($tokens as $token) {
-                if ($token['abc_token'] === $abc) {
-                    $found = true;
-                    $this->assertEquals($row['cannt_token'], $token['cannt_token']);
-                    $this->assertEquals($row['bmw_token'], $token['bmw_token']);
-                }
-            }
-            $this->assertTrue($found, "ABC token $abc not found in token table");
+            $token = $td->getToken($abc);
+            $this->assertEquals($row['cannt_token'], $token['cannt_token']);
+            $this->assertEquals($row['bmw_token'], $token['bmw_token']);
+            $this->assertEquals($row['description'], $token['description']);
         }
     }
 
     public function testAddBmwTokenUpdatesExistingRow()
     {
-        $abc = 'A';
-        $bmw = 'BMW_A';
-        Dict2php::addOrUpdateToken($abc, null, $bmw, null);
-        $token = Dict2php::getToken($abc);
-        $this->assertEquals($bmw, $token['bmw_token']);
+        $td = new TokenDictionary();
+        $td->addOrUpdateToken('A', 'C', null, 'desc');
+        $td->addOrUpdateToken('A', null, 'BMW_A', 'desc2');
+        $token = $td->getToken('A');
+        $this->assertEquals('BMW_A', $token['bmw_token']);
+        $this->assertEquals('desc2', $token['description']);
     }
 
     public function testAddNewTokenCreatesRow()
     {
-        $abc = 'NEW';
-        $cannt = 'NEWC';
-        $bmw = 'NEWB';
-        $desc = 'New token';
-        Dict2php::addOrUpdateToken($abc, $cannt, $bmw, $desc);
-        $token = Dict2php::getToken($abc);
-        $this->assertEquals($cannt, $token['cannt_token']);
-        $this->assertEquals($bmw, $token['bmw_token']);
-        $this->assertEquals($desc, $token['description']);
+        $td = new TokenDictionary();
+        $td->addOrUpdateToken('NEW', 'NEWC', 'NEWB', 'New token');
+        $token = $td->getToken('NEW');
+        $this->assertEquals('NEWC', $token['cannt_token']);
+        $this->assertEquals('NEWB', $token['bmw_token']);
+        $this->assertEquals('New token', $token['description']);
     }
 
     public function testEditTokenPersistsChanges()
     {
-        $abc = 'EDIT';
-        Dict2php::addOrUpdateToken($abc, 'C', 'B', 'D');
-        Dict2php::addOrUpdateToken($abc, 'C2', 'B2', 'D2');
-        $token = Dict2php::getToken($abc);
+        $td = new TokenDictionary();
+        $td->addOrUpdateToken('EDIT', 'C', 'B', 'D');
+        $td->addOrUpdateToken('EDIT', 'C2', 'B2', 'D2');
+        $token = $td->getToken('EDIT');
         $this->assertEquals('C2', $token['cannt_token']);
         $this->assertEquals('B2', $token['bmw_token']);
         $this->assertEquals('D2', $token['description']);
@@ -59,45 +56,43 @@ class TokenDictionaryTest extends TestCase
 
     public function testDeleteTokenRemovesRow()
     {
-        $abc = 'DEL';
-        Dict2php::addOrUpdateToken($abc, 'C', 'B', 'D');
-        Dict2php::deleteToken($abc);
-        $token = Dict2php::getToken($abc);
+        $td = new TokenDictionary();
+        $td->addOrUpdateToken('DEL', 'C', 'B', 'D');
+        $td->deleteToken('DEL');
+        $token = $td->getToken('DEL');
         $this->assertNull($token);
     }
 
     public function testConversionLogic()
     {
-        $abc = 'X';
-        $cannt = 'Y';
-        $bmw = 'Z';
-        Dict2php::addOrUpdateToken($abc, $cannt, $bmw, null);
-        $this->assertEquals($cannt, Dict2php::convertAbcToCannt($abc));
-        $this->assertEquals($abc, Dict2php::convertBmwToAbc($bmw));
+        $td = new TokenDictionary();
+        $td->addOrUpdateToken('X', 'Y', 'Z', null);
+        $this->assertEquals('Y', $td->convertAbcToCannt('X'));
+        $this->assertEquals('X', $td->convertBmwToAbc('Z'));
     }
 
     public function testEdgeCases()
     {
+        $td = new TokenDictionary();
         $abc = 'S@!';
         $cannt = '';
         $bmw = str_repeat('B', 256);
-        Dict2php::addOrUpdateToken($abc, $cannt, $bmw, null);
-        $token = Dict2php::getToken($abc);
+        $td->addOrUpdateToken($abc, $cannt, $bmw, null);
+        $token = $td->getToken($abc);
         $this->assertEquals($cannt, $token['cannt_token']);
         $this->assertEquals($bmw, $token['bmw_token']);
     }
 
     public function testBulkOperations()
     {
+        $td = new TokenDictionary();
         $bulk = [
             ['abc' => 'B1', 'cannt' => 'C1', 'bmw' => 'BM1', 'desc' => 'D1'],
             ['abc' => 'B2', 'cannt' => 'C2', 'bmw' => 'BM2', 'desc' => 'D2'],
         ];
+        $td->bulkImport($bulk);
         foreach ($bulk as $row) {
-            Dict2php::addOrUpdateToken($row['abc'], $row['cannt'], $row['bmw'], $row['desc']);
-        }
-        foreach ($bulk as $row) {
-            $token = Dict2php::getToken($row['abc']);
+            $token = $td->getToken($row['abc']);
             $this->assertEquals($row['cannt'], $token['cannt_token']);
             $this->assertEquals($row['bmw'], $token['bmw_token']);
             $this->assertEquals($row['desc'], $token['description']);
