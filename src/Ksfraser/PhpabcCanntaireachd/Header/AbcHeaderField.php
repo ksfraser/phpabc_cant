21c23d8cd46f2d6fbf831f847bf80c2818881bab
Kevin Fraser <kevin@ksfraser.ca>
2025-08-30 16:30:19 -0600

    extract fixHeader to own class

--- src/Ksfraser/PhpabcCanntaireachd/Header/AbcHeaderField.php
+++ src/Ksfraser/PhpabcCanntaireachd/Header/AbcHeaderField.php
@@ -5,7 +5,12 @@ abstract class AbcHeaderField {
     protected $value = '';
     public static $label = '';
     public function __construct($value = '') { $this->value = $value; }
-    public function set($value) { $this->value = $value; }
+    public function set($value) 
+    { 
+        // Only set if not already set or empty
+        if( ! isset( $this->value ) || $this->value === ''  )
+            $this->value = $value; 
+    }
     public function get() { return $this->value; }
     public function render() { return static::$label . ':' . $this->value . "\n"; }
 }
--- src/Ksfraser/PhpabcCanntaireachd/Header/AbcHeaderU.php
+++ src/Ksfraser/PhpabcCanntaireachd/Header/AbcHeaderU.php
@@ -1,7 +1,7 @@
 <?php
 namespace Ksfraser\PhpabcCanntaireachd\Header;
 //require_once __DIR__ . '/AbcHeaderT.php';
-use Ksfraser\PhpabcCanntaireachd\Header\AbcHeaderMultiField
-class AbcHeaderC extends AbcHeaderMultiField {
+use Ksfraser\PhpabcCanntaireachd\Header\AbcHeaderMultiField;
+class AbcHeaderU extends AbcHeaderMultiField {
     public static $label = 'U';
 }
