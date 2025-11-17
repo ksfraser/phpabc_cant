fc3960ed9f12f886bd0b8c3f9cfa57e900b7b5bf
Kevin Fraser <fraser.ks@gmail.com>
2025-11-17 07:59:04 -0700

    Phase 5: Remove debug scripts, temporary output files, and obsolete test files (58 files cleaned)

--- test_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-echo 'Autoload loaded' . PHP_EOL;
-$ref = new ReflectionClass('Ksfraser\PhpabcCanntaireachd\CanntGenerator');
-echo 'File: ' . $ref->getFileName() . PHP_EOL;
-?>
--- test_gen.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$gen = new Ksfraser\PhpabcCanntaireachd\CanntGenerator();
-echo 'A -> ' . $gen->generateForNotes('A') . "\n";
-echo 'B -> ' . $gen->generateForNotes('B') . "\n";
-echo 'C -> ' . $gen->generateForNotes('C') . "\n";
-?>
